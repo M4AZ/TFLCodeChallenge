298beb64584c07fc77264e0b7e866cd593065155
Masood <mohd007masood@gmail.com>
2023-04-27 11:36:39 +0100

    changed comment

--- TFLCodeChallenge.RestClient/RoadStatus/IRoadStatusClient.cs
+++ TFLCodeChallenge.RestClient/RoadStatus/IRoadStatusClient.cs
@@ -5,20 +5,20 @@ namespace TFLCodeChallenge.RestClient.RoadStatus;
 
 
 /// <summary>
-/// A client for https://restcountries.com/
+/// A client for https://api.tfl.gov.uk/
 /// </summary>
 /// <remarks>
-/// See the <a href="https://restcountries.com/">API documentation</a> for more information.
+/// See the <a href="https://api.tfl.gov.uk/">API documentation</a> for more information.
 ///</remarks>
 public interface IRoadStatusClient
 {
     
     /// <summary>
-    /// Gets all countries 
+    /// Gets road status of a given road 
     /// This is an asynchronous method.
     /// </summary>
     /// <returns>
-    /// When successful, the response will contain the list of all the country names 
+    /// When successful, the response will contain the status of the road
     /// </returns>
     Task<ClientResponse<IEnumerable<RoadStatusResponseModel>>> RoadStatus(string code,string appId, string appKey);
     
